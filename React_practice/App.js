4bfcbf3f86acf4479ac99600d6048ca6d1e42548
Abhishek Bande <bandeabhishek36@gmail.com>
2024-07-07 23:55:16 +0530

    react A-2 Assigment code update

--- React_practice/App.js
+++ React_practice/App.js
@@ -1,23 +1,3 @@
-//Hello World using javascript
-// const h1 = document.createElement('h1');
-// h1.innerText = "Hello, World using javascript!"
-// const render = document.getElementById("root");
-// render.append(h1)
-
-//Hello World using react
-// const h1 = React.createElement("h1", {}, "Hello, world using React!");
-// const root = ReactDOM.createRoot(document.getElementById("root"))
-// root.render(h1);
-
-//Creating nested react element
-/*
-<div className="container">
-    <div className="sub-container">
-        <img src="" alt="" />
-        <h1>Creating nested react elements! </h1>
-    </div>
-</div>
-*/
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
