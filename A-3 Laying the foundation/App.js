ccef9b6849c1c0e692e8a298d05404ce5bd9d1ee
Abhishek Bande <bandeabhishek36@gmail.com>
2024-07-11 22:27:18 +0530

    A-3 Assignment Complete

---
+++ A-3 Laying the foundation/App.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import "./index.css"
+
+/*Question:
+
+<div className="title">
+    <h1>This is h1</h1>
+    <h2>This is h2</h2>
+    <h3>This is h3</h3>
+</div>
+
+*/
+
+//Using React
+// const heading = React.createElement("div", { className: "title" },
+//                     React.createElement('h1', {}, "This is h1"),
+//                     React.createElement('h2', {}, "This is h2"),
+//                     React.createElement('h3', {}, "This is h3"))
+
+//Using JSX
+// const heading = (<div className="title">
+//     <h1>This is h1</h1>
+//     <h2>This is h2</h2>
+//     <h3>This is h3</h3>
+// </div>)
+
+//Creating functional component for the same
+
+const Title = () => (
+    <h1 className="main-title">Main Heading</h1>
+)
+
+//Componenet Composition
+const Heading = () => (
+    <>
+        <Title />
+        <div className="title">
+            <h1 className="h1-heading">This is h1</h1>
+            <h2 className="h2-heading">This is h2</h2>
+            <h3 className="h3-heading">This is h3</h3>
+        </div>
+    </>
+)
+
+const Header = () => (
+    <>
+        <div className="header-container">
+            <i class="img-responsive fa-sharp fa-solid fa-blog"></i>
+            <div className="left-side">
+                <span>Home</span>
+                <span>About</span>
+                <span>Contact</span>
+            </div>
+            <div className="right-side">
+                <i class="login-image fa-solid fa-user"></i>
+            </div>
+        </div>
+        <Heading />
+    </>
+)
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Header />)
+
+//Use of react element
+// root.render(heading);
+
+//Use of react component
+//root.render(<Heading />) 
--- React_practice/App.js
+++ React_practice/App.js
@@ -1,13 +1,56 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const container = React.createElement("div", { className: "container" },
-    React.createElement("div", { className: "sub-container" },
-        [React.createElement("img", { src: "#", alt: "image" }),
-        React.createElement("h1", {}, "Creating nested react elements!")]))
+
+//React Element
+const heading = (<h1>
+    Welcome To react, World!
+</h1>)
+
+/*
+React Component-two ways of components 
+ 1. Class Based Components -old
+ 2. Functional Components-new
+
+ 1.Functional Components-
+    - name should be start with capital letter.
+    - A function which returns a peice of JSX called as funtional component.
+
+    Q. What is Componenet Composition
+    one componenet inside another called as componenet composition.
+ */
+
+const HeadingComponents = () => {
+    return <h1 className='heading'> Welcome To react, World! with Functional Componenets</h1>
+};
+//Another way useing curly brackets
+// const Title = () => {
+//     return (
+//         <h1>This title is for react</h1>)
+// }
+
+const Title = () => (
+    <h1>This title is for react</h1>)
+
+const Title2 = () => (
+    <h1>This title is calling Title() for react</h1>)
+
+const HeadingComponents2 = () => (
+    <div className="container">
+        {/* How to render functional componenet */}
+        <Title />
+        {Title2()}
+        <Title></Title>
+        {/* How to use react element */}
+        {heading}
+        <h1 className='heading'> Welcome To react, World! with Functional Componenets</h1>
+    </div>)
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(container);
+//How to render react element
+//root.render(heading);
+//How to render functional componenet
+root.render(<HeadingComponents2 />);
 
 
 
