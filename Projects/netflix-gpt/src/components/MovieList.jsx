f2691d8f16519453446600484da4cfd52ec14329
Abhishek Bande <bandeabhishek36@gmail.com>
2024-09-08 21:47:06 +0530

    netflixGPT updated

--- Projects/netflix-gpt/src/components/MovieList.jsx
+++ Projects/netflix-gpt/src/components/MovieList.jsx
@@ -1,41 +1,47 @@
-import React, { useRef, useEffect } from "react";
 import MovieCard from "./MovieCard";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const MovieList = ({ movieTypes, moviesList }) => {
-  const scrollContainerRef = useRef(null);
-
-  useEffect(() => {
-    const scrollContainer = scrollContainerRef.current;
-
-    // Function to handle horizontal scrolling with mouse wheel
-    const handleWheelScroll = (event) => {
-      event.preventDefault(); // Prevent the default vertical scroll behavior
-      scrollContainer.scrollLeft += event.deltaY * 2; // Adjust scroll speed (2 is for faster scrolling)
-    };
-
-    // Add the event listener for the mouse wheel
-    scrollContainer.addEventListener("wheel", handleWheelScroll);
-
-    // Cleanup the event listener on component unmount
-    return () => {
-      scrollContainer.removeEventListener("wheel", handleWheelScroll);
-    };
-  }, []);
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 7, // Adjust based on the number of cards you want to show at a time
+    slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+    ],
+  };
 
   return (
-    <div className="p-4">
+    <div className="p-4 ml-4">
       <h1 className="text-3xl pb-6 text-white">{movieTypes}</h1>
-      <div
-        ref={scrollContainerRef}
-        className="flex overflow-x-hidden scrollbar-none space-x-4 scroll-smooth"
-        style={{ scrollBehavior: "smooth" }} // Ensure smooth scrolling is enabled
-      >
+      <Slider {...settings}>
         {moviesList?.map((movie) => (
-          <div key={movie.id} className="flex-shrink-0 w-40 md:w-44">
+          <div key={movie.id} className="px-2">
             <MovieCard movieList={movie} />
           </div>
         ))}
-      </div>
+      </Slider>
     </div>
   );
 };
